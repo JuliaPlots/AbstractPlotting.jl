3e77841e3ffbec9b788e451568874260e5b11f14
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-11-29 11:15:52 +0000

    remove module name

--- src/theming.jl
+++ src/theming.jl
@@ -4,13 +4,13 @@ https://www.nature.com/articles/nmeth.1618?WT.ec_id=NMETH-201106
 =#
 
 const wong_colors = [
-    AbstractPlotting.RGB(230/255, 159/255, 0/255),
-    AbstractPlotting.RGB(86/255, 180/255, 233/255),
-    AbstractPlotting.RGB(0/255, 158/255, 115/255),
-    AbstractPlotting.RGB(240/255, 228/255, 66/255),
-    AbstractPlotting.RGB(0/255, 114/255, 178/255),
-    AbstractPlotting.RGB(213/255, 94/255, 0/255),
-    AbstractPlotting.RGB(204/255, 121/255, 167/255),
+    RGB(230/255, 159/255, 0/255),
+    RGB(86/255, 180/255, 233/255),
+    RGB(0/255, 158/255, 115/255),
+    RGB(240/255, 228/255, 66/255),
+    RGB(0/255, 114/255, 178/255),
+    RGB(213/255, 94/255, 0/255),
+    RGB(204/255, 121/255, 167/255),
 ]
 
 const default_palettes = Attributes(
