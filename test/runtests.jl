fa0a5535031788b28be52244162295f6ea317f78
Anshul Singhvi <asinghvi17@simons-rock.edu>
2019-09-02 13:21:32 -0400

    reactivate opengl checking

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Test
 const MINIMAL = get(ENV, "ABSTRACTPLOTTING_MINIMAL", "false")
 
 # does this machine have a OPENGL?
-const OPENGL = false # haskey(ENV, "OPENGL") || haskey(ENV, "GITLAB_CI") || try success(pipeline(`glxinfo`, `grep version`)) catch; false end # if it's Gitlab, it must be JuliaGPU
+const OPENGL = haskey(ENV, "OPENGL") || haskey(ENV, "GITLAB_CI") || try success(pipeline(`glxinfo`, `grep version`)) catch; false end # if it's Gitlab, it must be JuliaGPU
 OPENGL || (MINIMAL = "true")
 
 @show OPENGL MINIMAL
