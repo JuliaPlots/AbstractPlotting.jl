8e2ace0d4186d30dbac344bc1af10e0a725f9666
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-01-17 15:27:12 +0530

    lift the string into formatter

--- src/basic_recipes/title.jl
+++ src/basic_recipes/title.jl
@@ -8,13 +8,16 @@
 Add a title with content `string` to `scene`.  Pass a `Function` to the `formatter` kwarg
 if the value passed to `string` isn't actually a String.
 """
-function title(scene, string; align = (:center, :bottom), textsize = 30, parent = Scene(), formatter = string, kw...)
+function title(scene, tstring; align = (:center, :bottom), textsize = 30, parent = Scene(), formatter = string, kw...)
+    
+    string = to_node(tstring)
+    
     pos = lift(pixelarea(scene)) do area
         x = widths(area)[1] ./ 2
         Vec2f0(x, 10) # offset 10px, to give it some space
     end
     t = text(
-        string,
+        lift(formatter, string),
         position = pos,
         camera = campixel!,
         raw = true, align = align,
