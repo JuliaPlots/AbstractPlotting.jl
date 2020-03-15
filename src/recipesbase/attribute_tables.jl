9835a4267c4763433ae69960f2a8b08d1c9d343a
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-03-15 22:19:09 +0530

    Minimal Working Example

---
+++ src/recipesbase/attribute_tables.jl
@@ -0,0 +1,21 @@
+const makie_linetype = KW(
+    :auto => nothing,
+    :solid => nothing,
+    :dash => :dash,
+    :dot => :dot,
+    :dashdot => :dashdot,
+    :dashdotdot => [0.4, 0.2, 0.1, 0.2, 0.4]
+)
+
+makie_color(c) = convert(RGBA, c)
+
+const makie_seriestype_map = Dict{Symbol, Type}(
+    :path => AbstractPlotting.Lines,
+    :path3d => AbstractPlotting.Lines,
+    :scatter => AbstractPlotting.Scatter,
+    :linesegments => AbstractPlotting.LineSegments,
+    :heatmap => AbstractPlotting.Heatmap,
+    :image => AbstractPlotting.Image,
+    :spy => AbstractPlotting.Spy,
+    :surface => AbstractPlotting.Surface
+)
--- src/recipesbase/recipesbase_integration.jl
+++ src/recipesbase/recipesbase_integration.jl
@@ -1,5 +1,9 @@
 using RecipePipeline, Plots
 using AbstractPlotting: Scene, lines!
+import AbstractPlotting
+
+include("attribute_tables.jl")
+
 # Define overrides for RecipesPipeline hooks.
 
 function RecipePipeline._recipe_init!(sc::Scene, plotattributes, args)
@@ -24,37 +28,117 @@ function RecipePipeline._recipe_finish!(sc::Scene, plotattributes, args)
     return sc
 end
 
+RecipePipeline._process_userrecipe(plt::Scene, kw_list, recipedata) = RecipePipeline._process_userrecipe(Plots.Plot(), kw_list, recipedata)
+
+RecipesBase.apply_recipe(plotattributes::Plots.AKW, ::Type{T}, ::AbstractPlotting.Scene) where T = throw(MethodError("Unmatched plot type: $T")) # TODO: loosen this restriction and move to RecipesBase
+
+
 # Allow a series type to be plotted.
 
-RecipePipeline.is_st_supported(sc::Scene, st) = true
+RecipePipeline.is_st_supported(sc::Scene, st) = haskey(makie_seriestype_map, st)
 
 RecipePipeline._preprocess_args(sc::Scene, args...) = RecipePipeline._preprocess_args(Plots.Plot(), args...)
 
+########################################
+#       Series argument slicing        #
+########################################
+
+function slice_arg(v::AbstractMatrix, idx::Int)
+    c = mod1(idx, size(v,2))
+    m, n = axes(v)
+    size(v, 1) == 1 ? v[first(m), n[c]] : v[:, n[c]]
+end
+
+# slice_arg(wrapper::InputWrapper, idx) = wrapper.obj
+
+slice_arg(v, idx) = v
+
+
+function makie_plottype(st::Symbol)
+    return get(makie_seriestype_map, st, AbstractPlotting.Lines)
+end
+
+function makie_args(::Union{Type{AbstractPlotting.Scatter}, Type{AbstractPlotting.Lines}}, plotattributes)
+    if haskey(plotattributes, :z)
+        return (plotattributes[:x], plotattributes[:y], plotattributes[:z])
+    else
+        return (plotattributes[:x], plotattributes[:y])
+    end
+end
+
+function translate_to_makie!(st, pa)
+    if st == :path || st == :path3d
+        if !isnothing(get!(pa, :line_z, nothing))
+            pa[:color] = pa[:line_z]
+        elseif !isnothing(get!(pa, :linecolor, nothing))
+            pa[:color] = pa[:linecolor]
+        elseif !isnothing(get!(pa, :seriescolor, nothing))
+            pa[:color] = pa[:seriescolor]
+        end
+        pa[:linewidth] = get(pa, :linesize, 1)
+    elseif st == :scatter
+        if !isnothing(get!(pa, :marker_z, nothing))
+            pa[:color] = pa[:marker_z]
+        elseif !isnothing(get!(pa, :markercolor, nothing))
+            pa[:color] = pa[:markercolor]
+        elseif !isnothing(get!(pa, :seriescolor, nothing))
+            pa[:color] = pa[:seriescolor]
+        end
+    else
+        # some default transformations
+    end
+end
+
+########################################
+#      The real plotting function      #
+########################################
+
+# TODO this only works for scatter
 function RecipePipeline.finalize_subplot!(plt::Scene, st, plotattributes)
     if st != :scatter
         @warn "What are you doing?"
     end
 
+    pt = makie_plottype(st)
+
+    theme = AbstractPlotting.default_theme(plt, pt)
+
+    translate_to_makie!(st, plotattributes)
+
     for (k, v) in pairs(plotattributes)
         isnothing(v) && delete!(plotattributes, k)
     end
 
-    @show plotattributes
+    args = makie_args(pt, plotattributes)
 
-    if !haskey(plotattributes, :z)
-        @info "No z"
-        AbstractPlotting.scatter!(plt, plotattributes[:x], plotattributes[:y]; plotattributes...)
-    else
-        @info "Z"
-        AbstractPlotting.scatter!(plt, plotattributes[:x], plotattributes[:y], plotattributes[:z]; plotattributes...)
+    # @infiltrate
+
+    for (k, v) in pairs(plotattributes)
+        haskey(theme, k) || delete!(plotattributes, k)
     end
+
+    # @infiltrate
+
+    AbstractPlotting.plot!(plt, pt, AbstractPlotting.Attributes(plotattributes), args...)
+
+    return plt
 end
 
 
-RecipePipeline._process_userrecipe(plt::Scene, kw_list, recipedata) = RecipePipeline._process_userrecipe(Plots.Plot(), kw_list, recipedata)
-
-RecipesBase.apply_recipe(plotattributes::Plots.AKW, ::Type{T}, ::AbstractPlotting.Scene) where T = throw(MethodError("Unmatched plot type: $T"))
-
 sc = Scene()
 
-RecipePipeline.recipe_pipeline!(sc, Dict(:color => :blue, :seriestype => :scatter), (1:10, 1:10))
+# AbstractPlotting.scatter!(sc, rand(10))
+RecipePipeline.recipe_pipeline!(sc, Dict(:seriestype => :scatter), (1:10, rand(10, 2)))
+
+RecipePipeline.recipe_pipeline!(sc, Dict(:color => :blue, :seriestype => :path), (1:10, rand(10, 1)))
+
+using DifferentialEquations, RecipePipeline, Makie
+import Plots # we need some recipes from here
+
+f(u,p,t) = 1.01.*u
+u0 = [1/2, 1]
+tspan = (0.0,1.0)
+prob = ODEProblem(f,u0,tspan)
+sol = solve(prob, Tsit5(), reltol=1e-8, abstol=1e-8)
+
+RecipePipeline.recipe_pipeline!(Scene(), Dict{Symbol, Any}(), (sol,))
