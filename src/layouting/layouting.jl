5d18d9be84f1071b7bd5ad1062c3f9e9d276c81f
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2020-12-06 15:01:18 +0100

    simplify layouting code a bit

--- src/layouting/layouting.jl
+++ src/layouting/layouting.jl
@@ -60,7 +60,7 @@ function layout_text(
     rscale = to_textsize(textsize)
     rot = to_rotation(rotation)
 
-    atlas = get_texture_atlas()
+    # atlas = get_texture_atlas()
     # mpos = model * Vec4f0(to_ndim(Vec3f0, startpos, 0f0)..., 1f0)
     # pos = to_ndim(Point3f0, mpos, 0)
 
@@ -96,34 +96,45 @@ function glyph_positions(str::AbstractString, font_per_char, fontscale_px, halig
         error("Invalid halign $halign. Valid values are <:Number, :left, :center and :right.")
     end
 
-    char_font_scale = collect(zip([c for c in str], font_per_char, fontscale_px))
-
-    linebreak_indices = (i for (i, c) in enumerate(str) if c == '\n')
-
-    groupstarts = [1; linebreak_indices .+ 1]
-    groupstops = [linebreak_indices .- 1; length(str)]
-
-    cfs_groups = map(groupstarts, groupstops) do start, stop
-        char_font_scale[start:stop]
-    end
-
-    extents = map(cfs_groups) do group
+    charinfos = broadcast([c for c in str], font_per_char, Ref(fontscale_px)) do char, font, scale
         # TODO: scale as SVector not Number
-        [get_extent(font, char) .* SVector(scale, scale) for (char, font, scale) in group]
+        extent = get_extent(font, char) .* SVector(scale, scale)
+        lineheight = Float32(font.height / font.units_per_EM * lineheight_factor * scale)
+        unscaled_hi_bb = height_insensitive_boundingbox(extent, font)
+        hi_bb = FRect2D(
+            AbstractPlotting.origin(unscaled_hi_bb) * scale,
+            widths(unscaled_hi_bb) * scale)
+        (char = char, font = font, scale = scale, extent = extent, hadvance = hadvance(extent),
+            hi_bb = hi_bb, lineheight = lineheight)
     end
 
-    # we need the advances for correct cursor placement
-    hadvances = map(extents) do extgroup
-        hadvance.(extgroup)
+    lineinfos = let 
+        last_line_start = 1
+        lineinfos = typeof(view(charinfos, last_line_start:last_line_start))[]
+        for (i, ci) in enumerate(charinfos)
+            if ci.char == '\n' || i == length(charinfos)
+                push!(lineinfos, view(charinfos, last_line_start:i))
+                last_line_start = i+1
+            end
+        end
+        lineinfos
     end
+
+    # for line in lineinfos
+    #     @show [l.char for l in line]
+    # end
+
 
     # add or subtract kernings?
-    xs = map(extents) do extgroup
-        cumsum([isempty(extgroup) ? 0.0 : -leftinkbound(extgroup[1]); hadvance.(extgroup[1:end-1])])
+    xs = map(lineinfos) do line        
+        cumsum([
+            isempty(line) ? 0.0 : -leftinkbound(line[1].extent);
+            [l.hadvance for l in line[2:end-1]]
+        ])
     end
 
     # each linewidth is the last origin plus inkwidth
-    linewidths = last.(xs) .+ [isempty(extgroup) ? 0.0 : inkwidth(extgroup[end]) for extgroup in extents]
+    linewidths = last.(xs) .+ [isempty(line) ? 0.0 : inkwidth(line[end].extent) for line in lineinfos]
     maxwidth = maximum(linewidths)
 
     width_differences = maxwidth .- linewidths
@@ -133,18 +144,11 @@ function glyph_positions(str::AbstractString, font_per_char, fontscale_px, halig
     end
 
     # make lineheight a multiple of the largest lineheight in each line
-    lineheights = map(cfs_groups) do group
-        # guard from empty reduction
-        isempty(group) && return 0f0
-
-        maximum(group) do (char, font, scale)
-            Float32(font.height / font.units_per_EM * lineheight_factor * scale)
-        end
+    lineheights = map(lineinfos) do line
+        maximum(l -> l.lineheight, line)
     end
 
-    # how to define line height relative to font size?
-    ys = cumsum([0; -lineheights[2:end]])
-
+    ys = cumsum([0.0; -lineheights[2:end]])
 
     # x alignment
     xs_aligned = [xsgroup .- halign * maxwidth for xsgroup in xs_justified]
@@ -153,12 +157,12 @@ function glyph_positions(str::AbstractString, font_per_char, fontscale_px, halig
     # first_max_ascent = maximum(hbearing_ori_to_top, extents[1])
     # last_max_descent = maximum(x -> inkheight(x) - hbearing_ori_to_top(x), extents[end])
 
-    first_line_ascender = maximum(cfs_groups[1]) do (char, font, scale)
-        ascender(font) * scale
+    first_line_ascender = maximum(lineinfos[1]) do l
+        ascender(l.font) * l.scale
     end
 
-    last_line_descender = minimum(cfs_groups[end]) do (char, font, scale)
-        descender(font) * scale
+    last_line_descender = minimum(lineinfos[end]) do l
+        descender(l.font) * l.scale
     end
 
     overall_height = first_line_ascender - ys[end] - last_line_descender
@@ -181,21 +185,12 @@ function glyph_positions(str::AbstractString, font_per_char, fontscale_px, halig
         ys .- first_line_ascender .+ (1 - va) .* overall_height
     end
 
-    height_insensitive_bbs = map(cfs_groups, extents) do group, extent
-        map(group, extent) do (char, font, scale), ext
-            unscaled_hi_bb = height_insensitive_boundingbox(get_extent(font, char), font)
-            FRect2D(AbstractPlotting.origin(unscaled_hi_bb) * scale, widths(unscaled_hi_bb) * scale)
-        end
-    end
-
     charorigins = [Ref(rotation) .* Point3f0.(xsgroup, y, 0) for (xsgroup, y) in zip(xs_aligned, ys_aligned)]
 
-    # concantenate all line-related vectors into one. fill info for '\n' chars with NaN data, doesn't matter
-    charorigins_vec = padded_vcat(charorigins, Point3f0(NaN))
-    height_insensitive_bbs_vec = padded_vcat(height_insensitive_bbs, FRect2D())
-    hadvances_vec = padded_vcat(hadvances, NaN)
-
-    return Glyphlayout(charorigins_vec, height_insensitive_bbs_vec, hadvances_vec)
+    return Glyphlayout(
+        reduce(vcat, charorigins),
+        reduce(vcat, map(line -> [l.hi_bb for l in line], lineinfos)),
+        reduce(vcat, map(line -> [l.hadvance for l in line], lineinfos)))
 end
 
 # function to concatenate vectors with a value between every pair
