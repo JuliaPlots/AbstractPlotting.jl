1986583143d2f97d1ccb50b1c7da782b86895bad
jkrumbiegel <22495855+jkrumbiegel@users.noreply.github.com>
2020-12-07 12:35:57 +0100

    rename frect3d

--- src/layouting/data_limits.jl
+++ src/layouting/data_limits.jl
@@ -109,25 +109,25 @@ function text_limits(x::AbstractVector)
     return FRect3D(x)
 end
 
-frect3d(p::Point2) = FRect3D(Point3f0(p..., 0), Point3f0(0, 0, 0))
-frect3d(p::Point3) = FRect3D(Point3f0(p...), Point3f0(0, 0, 0))
+FRect3D_from_point(p::VecTypes{2}) = FRect3D(Point3f0(p..., 0), Point3f0(0, 0, 0))
+FRect3D_from_point(p::VecTypes{3}) = FRect3D(Point3f0(p...), Point3f0(0, 0, 0))
 
 function atomic_limits(x::Text{<: Tuple{Arg1}}) where Arg1
     if x.space[] == :data
         return boundingbox(x)
     elseif x.space[] == :screen
         if x[1][] isa AbstractArray
-            bb = frect3d(x.position[][1])
+            bb = FRect3D_from_point(x.position[][1])
             for p in x.position[][2:end]
-                bb = union(bb, frect3d(p))
+                bb = union(bb, FRect3D_from_point(p))
             end
         else
             if x.position[] isa Union{StaticArrays.StaticArray, Tuple{Real, Real}, GeometryBasics.Point}
-                bb = frect3d(x.position[])
+                bb = FRect3D_from_point(x.position[])
             else
-                bb = frect3d(x.position[][1])
+                bb = FRect3D_from_point(x.position[][1])
                 for p in x.position[][2:end]
-                    bb = union(bb, frect3d(p))
+                    bb = union(bb, FRect3D_from_point(p))
                 end
                 bb
             end
