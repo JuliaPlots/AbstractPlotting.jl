fd9dd4259c1e6e230076be4280c1c258e3f2e364
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2021-01-08 17:17:16 +0100

    add a with_theme function for using a theme temporarily

--- src/theming.jl
+++ src/theming.jl
@@ -104,3 +104,32 @@ end
 function set_theme!(;kw_args...)
     set_theme!(Attributes(; kw_args...))
 end
+
+"""
+    with_theme(f, theme = Theme(); kwargs...)
+
+Calls `f` with `theme` temporarily activated. Attributes in `theme`
+can be overridden or extended with `kwargs`. The previous theme is always
+restored afterwards, no matter if `f` succeeds or fails.
+
+Example:
+
+```julia
+my_theme = Theme(resolution = (500, 500), color = :red)
+with_theme(my_theme, color = :blue, linestyle = :dashed) do
+    scatter(randn(100, 2))
+end
+```
+"""
+function with_theme(f, theme = Theme(); kwargs...)
+    temp_theme = merge!(Theme(kwargs), deepcopy(theme))
+    previous_theme = AbstractPlotting.current_default_theme()
+    try
+        set_theme!(temp_theme)
+        f()
+    catch e
+        rethrow(e)
+    finally
+        set_theme!(previous_theme)
+    end
+end
