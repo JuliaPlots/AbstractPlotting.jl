4cc84bcfe7d9692b7d3eee1676c46f1dcbf4758c
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2021-01-20 21:20:04 +0100

    reorder wong colors and default markers to be more pleasing

--- src/theming.jl
+++ src/theming.jl
@@ -38,18 +38,18 @@ https://www.nature.com/articles/nmeth.1618?WT.ec_id=NMETH-201106
 =#
 
 const wong_colors = [
-    RGB(230/255, 159/255, 0/255),
-    RGB(86/255, 180/255, 233/255),
-    RGB(0/255, 158/255, 115/255),
-    RGB(240/255, 228/255, 66/255),
-    RGB(0/255, 114/255, 178/255),
-    RGB(213/255, 94/255, 0/255),
-    RGB(204/255, 121/255, 167/255),
+    RGB(0/255, 114/255, 178/255), # dark blue
+    RGB(213/255, 94/255, 0/255), # red
+    RGB(0/255, 158/255, 115/255), # green
+    RGB(230/255, 159/255, 0/255), # orange
+    RGB(86/255, 180/255, 233/255), # light blue
+    RGB(204/255, 121/255, 167/255), # pink
+    RGB(240/255, 228/255, 66/255), # yellow
 ]
 
 const default_palettes = Attributes(
     color = wong_colors,
-    marker = [:circle, :xcross, :utriangle, :diamond, :dtriangle, :star8, :pentagon, :rect],
+    marker = [:circle, :rect, :utriangle, :dtriangle, :diamond, :star8, :pentagon, :xcross],
     linestyle = [nothing, :dash, :dot, :dashdot, :dashdotdot],
     side = [:left, :right]
 )
