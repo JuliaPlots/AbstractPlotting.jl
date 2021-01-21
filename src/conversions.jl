ec7d57f737548dd85be94adefee8bce08019fed4
Argel Ramírez Reyes <argel.ramirez@gmail.com>
2021-01-21 01:36:30 -0800

    Expanded convert function to take missing values

--- src/conversions.jl
+++ src/conversions.jl
@@ -260,7 +260,7 @@ outputs them in a Tuple.
 function convert_arguments(::SurfaceLike, x::AbstractVecOrMat{<: Number}, y::AbstractVecOrMat{<: Number}, z::AbstractMatrix{<: Union{Number, Colorant}})
     return (el32convert(x), el32convert(y), el32convert(z))
 end
-function convert_arguments(::SurfaceLike, x::AbstractVecOrMat{<: Number}, y::AbstractVecOrMat{<: Number}, z::AbstractMatrix{<:Number})
+function convert_arguments(::SurfaceLike, x::AbstractVecOrMat{<: Number}, y::AbstractVecOrMat{<: Number}, z::AbstractMatrix{<:Union{Missing, Number}})
     return (el32convert(x), el32convert(y), el32convert(z))
 end
 
