b078987d7bdb7f13766d1582be3cd2aa12aa8847
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2021-02-22 19:23:56 +0100

    add docs

--- src/basic_recipes/stemplot.jl
+++ src/basic_recipes/stemplot.jl
@@ -1,3 +1,17 @@
+"""
+    stemplot(xs, ys, [zs]; kwargs...)
+
+Plots markers at the given positions extending from `offset` along stem lines.
+
+`offset` can be a number, in which case it sets y for 2D and also z for 3D stemplots.
+It can be a Point2 for 2D plots and 3D plots, as well as a Point3 for 3D plots.
+It can also be an iterable of any of these at the same length as xs, ys, zs.
+
+The conversion trait of stemplot is `PointBased`.
+
+## Attributes
+$(ATTRIBUTES)
+"""
 @recipe(StemPlot) do scene
     Attributes(
         stemcolor = :black,
