c7032e5ecd17469031f281c24b2a61a2324a98e5
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2021-03-20 09:41:14 +0100

    enhance tests

--- test/text.jl
+++ test/text.jl
@@ -7,7 +7,7 @@ function heatmap_with_labels()
     fig = Figure(resolution = (600, 600))
     ax = fig[1, 1] = Axis(fig)
     tightlimits!(ax)
-    values = randn(100)
+    values = rand(100)
 
     poly!(ax, [FRect2D(x, y, 1, 1) for x in 1:10 for y in 1:10], color = values,
         strokecolor = :black, strokewidth = 1)
@@ -16,7 +16,8 @@ function heatmap_with_labels()
         string.(round.(values, digits = 2)),
         position = [Point2f0(x, y) .+ 0.5 for x in 1:10 for y in 1:10],
         align = (:center, :center),
-        textsize = 1)
+        color = ifelse.(values .< 0.3, :white, :black),
+        textsize = 12)
     display(fig)
 end
 
@@ -28,7 +29,7 @@ begin
         ["0 is the ORIGIN of this", "10 says hi"],
         position = pos,
         aspect = DataAspect(),
-        # space = :screen,
+        space = :data,
         align = (:center, :center),
         textsize = 2)
     scatter!(pos)
@@ -89,18 +90,20 @@ end
 multi_strings_multi_positions()
 
 
-function single_string_multi_positions()
-    scene = Scene(camera = campixel!, show_axis = false, resolution = (800, 800))
+# this is not implemented for now
 
-    str = "multistring"
-    points = decompose(Point2f0, Circle(Point2f0(400, 400), 200f0), length(str))
+# function single_string_multi_positions()
+#     scene = Scene(camera = campixel!, show_axis = false, resolution = (800, 800))
 
-    text!(scene, str, position = points)
+#     str = "multistring"
+#     points = decompose(Point2f0, Circle(Point2f0(400, 400), 200f0), length(str))
 
-    scene
-end
+#     text!(scene, str, position = points)
 
-single_string_multi_positions()
+#     scene
+# end
+
+# single_string_multi_positions()
 
 
 function single_strings_single_positions_justification()
@@ -110,18 +113,63 @@ function single_strings_single_positions_justification()
     scatter!(scene, points, marker = :circle, markersize = 10px)
 
     i = 1
-    for halign in (:right, :center, :left), justification in (0, 0.5, 1)
+    for halign in (:left, :center, :right), justification in (:left, :center, :right)
 
-        text!(scene, "First\nSecond\nThird",
+        t = text!(scene, "AA\nBBB\nCCCC\nDDDD",
             color = (:black, 0.5),
             position = points[i],
             align = (halign, :center),
+            space = :data,
             justification = justification)
 
+        bb = boundingbox(t)
+        wireframe!(scene, bb, color = (:red, 0.2))
+
         i += 1
+    end
+
+    for (p, al) in zip(points[3:3:end], (:left, :center, :right))
+        text!(scene, "align " * string(al), position = p .+ (0, 80),
+            align = (:center, :baseline))
+    end
+
+    for (p, al) in zip(points[7:9], (:left, :center, :right))
+        text!(scene, "justif " * string(al), position = p .+ (80, 0),
+            align = (:center, :baseline), rotation = pi/2)
     end
 
     scene
 end
 
 single_strings_single_positions_justification()
+
+
+function multi_boundingboxes()
+    scene = Scene(camera = campixel!, show_axis = false, resolution = (800, 800))
+
+    t1 = text!(scene,
+        fill("makie", 8),
+        position = [(200, 200) .+ 60 * Point2f0(cos(a), sin(a)) for a in 0:pi/4:7pi/4],
+        rotation = 0:pi/4:7pi/4,
+        align = (:left, :center),
+        textsize = 30,
+        space = :data
+    )
+
+    wireframe!(scene, boundingbox(t1), color = (:blue, 0.3))
+
+    t2 = text!(scene,
+        fill("makie", 8),
+        position = [(200, 600) .+ 60 * Point2f0(cos(a), sin(a)) for a in 0:pi/4:7pi/4],
+        rotation = 0:pi/4:7pi/4,
+        align = (:left, :center),
+        textsize = 30,
+        space = :screen
+    )
+
+    wireframe!(scene, boundingbox(t2), color = (:red, 0.3))
+
+    scene
+end
+
+multi_boundingboxes()
