406673a8c6444578ebd4c8821b1fe44c2842fa42
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2021-04-09 12:13:46 +0200

    better example

--- paper_example.jl
+++ paper_example.jl
@@ -1,11 +1,10 @@
 using CairoMakie
-CairoMakie.activate!(type = "png")
 
 polynomials = ["20x", "3x^2 + 3x", "x^3 - 2x^2 - 10x"]
 functions = eval.(Meta.parse.("x -> " .* polynomials))
 
-f = Figure(resolution = (700, 900), fontsize = 12)
-ax = Axis(f[1, 1], xlabel = "x", ylabel = "f(x)", title = "Polynomials")
+f = Figure(resolution = (700, 500), fontsize = 14, font = "Helvetica")
+ax = Axis(f[2, 1], xlabel = "x", ylabel = "f(x)", title = "Polynomials")
 
 colors = [:tomato, "#04e04c", RGBf0(0.1, 0.3, 1)]
 
@@ -13,7 +12,7 @@ for (f, p, color) in zip(functions, polynomials, colors)
     lines!(-5..5, f, label = p, color = color, linewidth = 2)
 end
 
-Legend(f[1, 2], ax, "f(x) =")
+Legend(f[1, 1], ax, orientation = :horizontal, colgap = 20, tellheight = true)
 
 function mandelbrot(x, y)
     z = c = x + y*im
@@ -23,11 +22,14 @@ function mandelbrot(x, y)
     return 0.0
 end
 
-ax2, hm = heatmap(f[2, 1:2], -2:0.01:1, -2:0.01:2, mandelbrot,
-    interpolate = true, axis = (height = 400, title = "Mandelbrot"))
+ax2, hm = heatmap(f[1:2, 2][1, 1], -2:0.01:1, -2:0.01:2, mandelbrot,
+    interpolate = true, colormap = :thermal)
 hidedecorations!(ax2)
-Colorbar(f[3, 1:2], hm, height = 20, vertical = false, flipaxis = false)
+Colorbar(f[1:2, 2][2, 1], hm, height = 20, vertical = false,
+    flipaxis = false, label = "Iterations")
+
+Label(f[0, :], "Makie.jl Example Figure")
 
 f
-
+##
 save("paper_example.png", f, px_per_unit = 2)
