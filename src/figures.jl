df4887226d8d5fe5b247386bdf20edb095cd8c9d
David Widmann <dev+github@devmotion.de>
2021-04-13 12:37:12 +0200

    Revert "Replace `only` with `first`"
    This reverts commit df4674300bfc2c057f2703b6ebfe8d4fdebec364.

--- src/figures.jl
+++ src/figures.jl
@@ -147,7 +147,7 @@ function get_layout_at!(fp::FigurePosition; createmissing = false)
     if isempty(layouts) && createmissing
         fp.gp[] = GridLayoutBase.GridLayout()
     elseif length(layouts) == 1
-        first(layouts)::GridLayoutBase.GridLayout
+        only(layouts)::GridLayoutBase.GridLayout
     else
         nothing
     end
@@ -167,7 +167,7 @@ function get_layout_at!(fsp::FigureSubposition; createmissing = false)
     if isempty(layouts) && createmissing
         gp[] = GridLayoutBase.GridLayout()
     elseif length(layouts) == 1
-        first(layouts)::GridLayoutBase.GridLayout
+        only(layouts)::GridLayoutBase.GridLayout
     else
         nothing
     end
