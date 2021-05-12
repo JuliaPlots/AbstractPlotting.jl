6fa1d5fd3af7a904982bf2fb9b9af70dbf28c654
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2021-05-12 21:50:10 +0200

    fix default palette

--- src/theming.jl
+++ src/theming.jl
@@ -32,19 +32,23 @@ Conservative 7-color palette from Points of view: Color blindness, Bang Wong - N
 https://www.nature.com/articles/nmeth.1618?WT.ec_id=NMETH-201106
 =#
 
-const wong_colors = [
-    RGB(230/255, 159/255, 0/255),
-    RGB(86/255, 180/255, 233/255),
-    RGB(0/255, 158/255, 115/255),
-    RGB(240/255, 228/255, 66/255),
-    RGB(0/255, 114/255, 178/255),
-    RGB(213/255, 94/255, 0/255),
-    RGB(204/255, 121/255, 167/255),
-]
+function wong_colors(alpha)
+    colors = [
+        RGB(0/255, 114/255, 178/255), # blue
+        RGB(230/255, 159/255, 0/255), # orange
+        RGB(0/255, 158/255, 115/255), # green
+        RGB(204/255, 121/255, 167/255), # reddish purple
+        RGB(86/255, 180/255, 233/255), # sky blue
+        RGB(213/255, 94/255, 0/255), # vermillion
+        RGB(240/255, 228/255, 66/255), # yellow
+    ]
+    @. RGBAf0(red(colors), green(colors), blue(colors), alpha)
+end
 
 const default_palettes = Attributes(
-    color = wong_colors,
-    marker = [:circle, :xcross, :utriangle, :diamond, :dtriangle, :star8, :pentagon, :rect],
+    color = wong_colors(1),
+    patchcolor = AbstractPlotting.wong_colors(0.6),
+    marker = [:circle, :utriangle, :cross, :rect, :diamond, :dtriangle, :pentagon, :xcross],
     linestyle = [nothing, :dash, :dot, :dashdot, :dashdotdot],
     side = [:left, :right]
 )
